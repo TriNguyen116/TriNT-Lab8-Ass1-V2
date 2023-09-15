a6411f3e24760eae1a47491934e2484b59cc53c0
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-09-16 00:12:51 +0700

    Lab4-UpdateMenuComponent

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null
+    };
+  }
   render() {
     return (
-      <FlatList data={this.props.dishes}
+      <View style={{ flex: 1 }}>
+        <FlatList data={this.props.dishes}
         renderItem={({ item, index }) => this.renderMenuItem(item, index)}
         keyExtractor={(item) => item.id.toString()} />
+        <Dishdetail dish={this.state.selectedDish} />
+      </View>
     );
   }
   renderMenuItem(item, index) {
     return (
-      <ListItem key={index}>
+      <ListItem key={index} onPress={() => this.onDishSelect(item)}>
         <Avatar source={require('./images/uthappizza.png')} />
         <ListItem.Content>
           <ListItem.Title>{item.name}</ListItem.Title>
@@ -20,6 +30,9 @@ class Menu extends Component {
         </ListItem.Content>
       </ListItem>
     );
+  }
+  onDishSelect(item) {
+    this.setState({ selectedDish: item });
   }
 }
 export default Menu;
