ed2ee5bc84be0250e3073950799b6550ca15fd18
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-09-15 20:04:58 +0700

    Lab2-HelloComponent

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import Hello from './components/HelloComponent';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello TriNTqqq</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FD9186',
-  },
-
-  text: {
-    color: 'white',
-    fontSize: '40px',
-    fontWeight: '700',
+class App extends Component {
+  render() {
+    return (
+      <Hello name = 'TriNT'/>
+    );
   }
-});
+}
+export default App;
+
+
 
