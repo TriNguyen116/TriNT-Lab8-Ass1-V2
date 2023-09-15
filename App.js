e3a7fa8a04f83fb786307ee50de7b5878ec0f658
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-09-15 18:25:02 +0700

    Lab1-15/9-HelloWorld-expo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello TriNT</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FD9186',
   },
+
+  text: {
+    color: 'white',
+    fontSize: '40px',
+    fontWeight: '700',
+  }
 });
+
