36f4e91efbed0626049110245bc868d45d7adede
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-09-24 14:23:49 +0700

    use-component-more

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,57 @@ import { color } from "react-native-elements/dist/helpers";
 import { ListItem, Avatar } from 'react-native-elements';
 import {LEADERS} from '../shared/leaders';
 
+
+  class RenderHistory extends Component {
+    render() {
+      return(
+      <Card>
+            <View style={styles.lineTitle}>
+              <Text style={styles.title}>Our History</Text>
+            </View>
+            <View style={styles.flexText}>
+              <Text style={styles.text}>Started in 2010, Ristorante con Fusion quickly established itself as 
+              a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that 
+              can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring 
+              four of the best three-star Michelin chefs in the world, you never know what will arrive on your 
+              plate the next time you visit us.</Text>
+              <Text style={styles.text}>The restaurant traces its humble beginnings to The Frying Pan, a successful 
+              chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+            </View>
+        </Card>
+      )
+    }
+  }
+
+  class RenderAboutItem extends Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      const { leaders } = this.props;
+      return(
+        <Card>
+        <View style={styles.lineTitle}>
+          <Text style={styles.title}>Corporate Leadership</Text>
+        </View>
+          <FlatList data={leaders}  renderItem={({ item, index }) => this.renderAboutItem(item, index)}
+          keyExtractor={(item) => item.id.toString()} />  
+      </Card>
+      )
+    }
+
+    renderAboutItem(item, index) {
+      return (
+        <ListItem key={index}>
+          <Avatar rounded size={45} source={require('./images/alberto.png')} />
+          <ListItem.Content>
+            <ListItem.Title style={styles.nameTitle}>{item.name}</ListItem.Title>
+            <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      );
+    }
+  }
 class About extends Component {
   constructor(props) {
     super(props);
@@ -14,45 +65,13 @@ class About extends Component {
   }
   render() {
     return (
-      <ScrollView style={styles.scrollView}>
-      <Card>
-          <View style={styles.lineTitle}>
-            <Text style={styles.title}>Our History</Text>
-          </View>
-          <View style={styles.flexText}>
-            <Text style={styles.text}>Started in 2010, Ristorante con Fusion quickly established itself as 
-            a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that 
-            can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring 
-            four of the best three-star Michelin chefs in the world, you never know what will arrive on your 
-            plate the next time you visit us.</Text>
-            <Text style={styles.text}>The restaurant traces its humble beginnings to The Frying Pan, a successful 
-            chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
-          </View>
-      </Card>
-      <Card>
-      <View style={styles.lineTitle}>
-            <Text style={styles.title}>Corporate Leadership</Text>
-          </View>
-          <View style={styles.flexText}>
-            <FlatList data={this.state.leaders}  renderItem={({ item, index }) => this.renderAboutItem(item, index)}
-            keyExtractor={(item) => item.id.toString()} />
-          </View>
-      </Card>
+      <ScrollView >
+        <RenderHistory></RenderHistory>
+        <RenderAboutItem leaders={this.state.leaders}></RenderAboutItem>
       </ScrollView>
     )
   }
 
-  renderAboutItem(item, index) {
-    return (
-      <ListItem key={index}>
-        <Avatar rounded size={45} source={require('./images/alberto.png')} />
-        <ListItem.Content>
-          <ListItem.Title style={styles.nameTitle}>{item.name}</ListItem.Title>
-          <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
